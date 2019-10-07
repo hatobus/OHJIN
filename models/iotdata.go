fb145387d5b7a013f3eb0470dac8e92137ad6cb1
hatobus <s1240056@gmail.com>
2019-10-07 23:27:51 +0900

    lambda func for smartagri devices

--- models/iotdata.go
+++ models/iotdata.go
@@ -1,12 +1,11 @@
 package models
 
 type Iotdata struct {
-	No          int    `xorm:"not null pk autoincr INT(11)"`
-	Machineid   string `xorm:"not null VARCHAR(3)" json:"no"`
-	Temp        string `xorm:"VARCHAR(10)" json:"temperture"`
-	Humid       string `xorm:"VARCHAR(10)" json:"humidity"`
-	SoilHumid   string `xorm:"VARCHAR(10)" json:"soil_humidity"`
-	Co2         string `xorm:"VARCHAR(10)" json:"co2_concentration"`
-	Illuminance string `xorm:"VARCHAR(10)" json:"illuminance"`
-	Wavelength  string `xorm:"VARCHAR(10)" json:"wavelength"`
+	Machineid   string `json:"no"`
+	Temp        string `json:"temperture"`
+	Humid       string `json:"humidity"`
+	SoilHumid   string `json:"soil_humidity"`
+	Co2         string `json:"co2_concentration"`
+	Illuminance string `json:"illuminance"`
+	Wavelength  string `json:"wavelength"`
 }
